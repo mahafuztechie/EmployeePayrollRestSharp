324cf6e0da96d8999bf1bb40fb92528a577d8819
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-02-22 20:42:04 +0530

    [mahafuz] adding uc delete emp

--- EmployeeRestSharp/EmployeeRestSharp/RestSharpTestCases.cs
+++ EmployeeRestSharp/EmployeeRestSharp/RestSharpTestCases.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -50,6 +51,44 @@ namespace EmployeeRestSharp
             {
                 System.Console.WriteLine("id: " + emp.id + ", Name: " + emp.first_name + ", Salary: " + emp.salary);
             }
+        }
+
+        [TestMethod]
+        public void OnCallingPostAPI_ReturnEmployeeObject()
+        {
+            // Arrange
+            // endpoint and POST method
+            RestRequest request = new RestRequest("/employees", Method.Post);
+            JObject jObjectBody = new JObject();
+            jObjectBody.Add("first_name", "Clark");
+            jObjectBody.Add("salary", "15000");
+
+            // Added parameters to the request object, content-type and jObjectBody with the request
+            request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
+
+            //Act
+            RestResponse response = client.ExecuteAsync(request).Result;
+
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+            //Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            //Assert.AreEqual("Clark", dataResponse.first_name);
+            //Assert.AreEqual("15000", dataResponse.salary);
+            //System.Console.WriteLine(response.Content);
+        }
+
+        [TestMethod]
+        public void OnCallingDeleteAPI_ReturnSuccessStatus()
+        {
+            // Arrange
+            RestRequest request = new RestRequest("/employees/delete_emp/4", Method.Delete);
+
+            // Act
+            RestResponse response = client.ExecuteAsync(request).Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Console.WriteLine(response.Content);
         }
     }
 }
