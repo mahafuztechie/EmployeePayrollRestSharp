099300c27fece999f76f35e390a36d45d1d6654b
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-02-22 20:09:53 +0530

    [mahafuz] adding uc1 get all employee

---
+++ EmployeeRestSharp/EmployeeRestSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeRestSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeRestSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f45e0a4-9f34-437f-9072-06f80bdeb0ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EmployeeRestSharp/EmployeeRestSharp/RestSharpTestCases.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace EmployeeRestSharp
+{
+ 
+        public class Employee
+        {
+            public int id { get; set; }
+            public string first_name { get; set; }
+            public string salary { get; set; }
+      
+        }
+        [TestClass]
+        public class RestSharpTestCase
+        {
+            RestClient client;
+            [TestInitialize]
+            public void SetUp()
+            {
+                client = new RestClient("http://localhost:4000");
+            }
+
+            private RestResponse GetEmployeeList()
+            {
+                // arrange
+                RestRequest request = new RestRequest("/employees", Method.Get);
+
+            // act
+            RestResponse response = client.ExecuteAsync(request).Result;
+
+            return response;
+            }
+
+        [TestMethod]
+        public void onCallingGETApi_ReturnEmployeeList()
+        {
+            RestResponse response = GetEmployeeList();
+
+            // Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(3, dataResponse.Count);
+
+            foreach (Employee emp in dataResponse)
+            {
+                System.Console.WriteLine("id: " + emp.id + ", Name: " + emp.first_name + ", Salary: " + emp.salary);
+            }
+        }
+    }
+}
